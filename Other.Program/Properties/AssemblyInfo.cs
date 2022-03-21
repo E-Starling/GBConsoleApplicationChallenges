192957b591264e204f3e9ab3eb7433497094b208
Ethan Starling <ethan.jr.starling@gmail.com>
2022-03-21 17:11:36 -0400

    first commit

--- Other.Program/Properties/AssemblyInfo.cs
+++ Other.Program/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Program")]
+[assembly: AssemblyTitle("Other.Program")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Program")]
+[assembly: AssemblyProduct("Other.Program")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("73f51458-6af2-4fae-bc64-bc563e393fcc")]
+[assembly: Guid("594a3937-59c9-46fd-ac36-720b6d69e8c0")]
 
 // Version information for an assembly consists of the following four values:
 //
